bfcd305a3856b3094e174734a6e8fdb5932b5014
Colin Fendrick <colin.fendrick@gmail.com>
2021-01-05 08:22:46 -0800

    Add hashmap struct and impl method

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
 	let action = std::env::args().nth(1).expect("Please specify an action");
 	let item = std::env::args().nth(2).expect("Please specify an item");
@@ -5,3 +7,13 @@ fn main() {
 	println!("${:?}, ${:?}", action, item);
 }    
 
+struct Todo {
+	map: HashMap<String, bool>,
+}
+
+
+impl Todo {
+	fn insert(&mut self, key: String) {
+		self.map.insert(key, true);
+	}
+}
