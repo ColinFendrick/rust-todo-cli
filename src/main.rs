1b83c878a21d9564e9db03b4403fecea67b7feef
Colin Fendrick <colin.fendrick@gmail.com>
2021-01-05 08:32:06 -0800

    How to use the memory management to enforce code rules

--- src/main.rs
+++ src/main.rs
@@ -16,4 +16,23 @@ impl Todo {
 	fn insert(&mut self, key: String) {
 		self.map.insert(key, true);
 	}
+
+	fn save(self) -> Result<(), std::io::Error> {
+		/*
+			It's important to notice that save take ownership of self.
+			This is an arbitrary decision so that the compiler would stop us if we were
+			to accidentally try to update the map after we called save
+			(as the memory of self would be freed).
+
+			This means that save must be the last method used when writing this
+			program (or it won't compile). THis is how we use Rust's memory management
+			to create strict code!
+		*/
+		let mut content = String::new();
+		for (k, v) in self.map {
+			let record = format!("{}\t{}\n", k, v);
+			content.push_str(&record)
+		}
+		std::fs::write("db.txt", content)
+	}
 }
